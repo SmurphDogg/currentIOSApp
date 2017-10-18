38b1fe92de5522a636a4ac8b2d729579651589ae
Murphy Basil Ward <murphybasilward@murphys-imac.centurylink.net>
2017-10-17 19:34:59 -0500

    10-17-2017
    Programmatically created login screen in tableview without create profile

--- Current/CreateProfileTableViewController.swift
+++ Current/CreateProfileTableViewController.swift
@@ -19,7 +19,9 @@ struct cellData{
 class CreateProfileTableViewController: UITableViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   
-    var arrayOfCells = [cellData]()
+    var tableCellDataArray = [cellData]()
+   // var cellArray = [UITableViewCell]()
+   // var cellIndex = 0
     
     //https://makeapppie.com/2016/06/28/how-to-use-uiimagepickercontroller-for-a-camera-and-photo-library-in-swift-3-0/ use this tutorial to include photopicker
     let myImagePicker = UIImagePickerController()
@@ -27,8 +29,8 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        arrayOfCells = [cellData(cellType : 1, text : "Name"),
+        self.tableView.register(TextFieldTableViewCell  .self, forCellReuseIdentifier: "Cell")
+        tableCellDataArray = [cellData(cellType : 1, text : "Name"),
                         cellData(cellType : 2, text : "First Name"),
                         cellData(cellType : 2, text : "Middle Name"),
                         cellData(cellType : 2, text : "Last Name"),
@@ -36,6 +38,8 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
                         cellData(cellType : 1, text : "Pictures"),
                         cellData(cellType : 3, text : "Profile Pic"),
                         cellData(cellType : 3, text : "Anonymous Pic")]
+        
+        
         
         self.tableView.separatorStyle = .none
         // Uncomment the following line to preserve selection between presentations
@@ -59,7 +63,7 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return arrayOfCells.count
+        return tableCellDataArray.count
     }
 
     
@@ -69,31 +73,36 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
         
 
         //Headers of labels
-        if arrayOfCells[indexPath.row].cellType == 1{
+        if tableCellDataArray[indexPath.row].cellType == 1{
             let label = UILabel(frame : CGRect(x: 150.0, y: 14.0, width: 150.0, height: 30.0))
             label.font = UIFont (name: "Futura-Medium", size: 30)
-            label.text = arrayOfCells[indexPath.row].text
+            label.text = tableCellDataArray[indexPath.row].text
             label.tag = indexPath.row
             cell.contentView.addSubview(label)
             return cell
         }
         
         //Text Fields
-        if arrayOfCells[indexPath.row].cellType == 2{
+        if tableCellDataArray[indexPath.row].cellType == 2{
             
+            //Create Text Field
             let textField = UITextField(frame : CGRect(x: 10.0, y: 14.0, width: 200.0, height: 30.0))
             textField.font = UIFont (name: "Futura-Medium", size: 15)
-            textField.placeholder = arrayOfCells[indexPath.row].text
+            textField.placeholder = tableCellDataArray[indexPath.row].text
+            
+            //Tag determines the way to reference cells to pull out data to make a user
             textField.tag = indexPath.row
+            //add cells
             cell.contentView.addSubview(textField)
+            
             return cell
         }
       
         //Label and Button
-        if arrayOfCells[indexPath.row].cellType == 3{
+        if tableCellDataArray[indexPath.row].cellType == 3{
             //label describing button functionality
             
-            /*
+            /*If the button should have a label, include this
             let label = UILabel(frame : CGRect(x: 10.0, y: 14.0, width: 100.0, height: 30.0))
             label.font = UIFont (name: "Futura-Medium", size: 15)
             label.text = arrayOfCells[indexPath.row].text + ": "
@@ -104,36 +113,107 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
             let button = UIButton()
             button.frame = CGRect(x: 10, y: 0, width: 100, height: 25)
             button.backgroundColor = UIColor.lightGray
+            
+            //add rounded buttons
             button.layer.borderWidth = 1
             button.layer.cornerRadius = 5
             button.titleLabel?.font = UIFont (name: "Futura-Medium", size: 12)
-            button.setTitle(arrayOfCells[indexPath.row].text, for: .normal)
+            button.setTitle(tableCellDataArray[indexPath.row].text, for: .normal)
             button.addTarget(self, action: #selector(importImageButton(_:)), for: .touchUpInside)
-            self.view.addSubview(button)
             
+            //Tag helps reference location of cell
+            button.tag = indexPath.row
+            //Add button to cell
+            self.view.addSubview(button)
+            //Add cell to tableView
             cell.contentView.addSubview(button)
+            
             return cell
         }
 
+        //So that we at least don't crash
         print("You don't have a special cell")
         return cell
-            /*if let cell2 = cell as? TextTableViewCell{
-                print("You got a cell")
-                
-                cell2.myTextLabel.text = "Name"
-                return cell2}
-        
-        
-            print("basic cell")
-            return cell
-        */
-        //}
-        /*
-        */
        
     }
     
 
+    @IBAction func createCurrentUser(_ sender: Any) {
+        
+        //If every necessary field is filled out, create the user
+     //   let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+      //  let user = User(context: context) // Link Task & Context
+        print("1")
+        //As many sections-- Thiis line might be redundant since I only plan on doing one section
+        for section in 0..<tableView.numberOfSections {
+            //As many rows
+            for row in 0..<tableView.numberOfRows(inSection: section) {
+                let indexPath = NSIndexPath(row: row, section: section)
+                print(indexPath.row)
+                //let cell = tableView.cellForRow(at: indexPath as IndexPath)
+                
+                    switch(indexPath.row){
+                        
+                    case 1:
+                       let selectedCell = tableView!.cellForRow(at: indexPath as IndexPath) as! TextFieldTableViewCell//your custom cell class.
+       //                 let firstName = selectedCell.myTextField.text //textFied is your textfield name.
+                       // print(firstName ?? "loser")
+                        /*
+                        print("3 ")
+                            
+                       if let textFieldCell = cell as? TextFieldTableViewCell{
+                        var firstName = "default"
+                         firstName = textFieldCell.myTextField.text!
+                        //print(firstName)
+                          //print(firstName ?? firstName)
+                                print(firstName)
+                        */
+                        
+
+                        break;
+                        
+                    case 2:
+                        
+                        break;
+                        
+                    case 3:
+                        
+                        
+                        break;
+                        
+                    case 4:
+                        
+                        break;
+                        
+                        
+                        
+                    case 6:
+                        
+                        break;
+                        
+                    case 7:
+                        
+                        
+                        break;
+                        
+                    default: break
+                        
+                }
+                
+                
+                
+            }
+        }
+        
+        
+        
+        
+        
+        // Save the data to coredata
+   //     (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+     //   let _ = navigationController?.popViewController(animated: true)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -183,6 +263,9 @@ class CreateProfileTableViewController: UITableViewController,UIImagePickerContr
         print("Button tapped")
     }
     
+    func saveContext(){
+        
+    }
     
     @IBAction func cancelCreateProfile(_ sender: Any) {
         self.dismiss(animated: true) { 
